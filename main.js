8fbd07126d912061d996daeee86ecc0e207898e9
Ayoussefjoo <39112938+Ayoussefjoo@users.noreply.github.com>
2020-06-24 01:36:37 +0400

    high score text

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = 600;
+canvas.width = 400;
 canvas.height = 400;
 let spacePressed = false;
 let angle = 0;
@@ -42,10 +42,10 @@ window.addEventListener('keyup', function (e) {
     }
 
 })
-window.addEventListener('touchstart',function(e){
+window.addEventListener('touchstart', function (e) {
     spacePressed = true;
 })
-window.addEventListener('touchend',function(e){
+window.addEventListener('touchend', function (e) {
     spacePressed = false;
 })
 const bang = new Image();
@@ -62,20 +62,22 @@ function handleCollisions() {
 
 
             ctx.drawImage(bang, bird.x, bird.y, 50, 50);
-            ctx.font = "25px Georgia";
+            ctx.font = "15px Georgia";
             ctx.fillStyle = 'black';
 
             var highScore = JSON.parse(localStorage.getItem('highScoreBirds'));
             if (highScore) {
                 if (score > highScore)
                     highScore = score;
-               // localStorage.setItem('highScoreBirds', JSON.stringify(highScore));
-               // ctx.fillText('Game Over,High Score' + highScore + '   your score is: ' + score, 160, canvas.height / 2 - 10);
-            }else{
-highScore = score;
-}
+                // localStorage.setItem('highScoreBirds', JSON.stringify(highScore));
+                // ctx.fillText('Game Over,High Score' + highScore + '   your score is: ' + score, 160, canvas.height / 2 - 10);
+            }
             localStorage.setItem('highScoreBirds', JSON.stringify(highScore));
-            ctx.fillText('Game Over,High Score' + highScore + '   your score is: ' + score, 160, canvas.height / 2 - 10);
+            ctx.fillText('Game Over,your Score is: ' + score, 160, canvas.height / 2 - 10);
+            if(highScore==null){
+                highScore=0;
+            }
+            ctx.fillText('High Score: ' + highScore, 160, canvas.height / 2 + 30)
             return true;
         }
     }
