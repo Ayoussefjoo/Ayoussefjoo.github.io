180a0fbecae3166ebcd28021c55f99c3cbdb5b23
Ayoussefjoo <39112938+Ayoussefjoo@users.noreply.github.com>
2020-06-23 14:49:41 +0400

    save in local stoarge

--- main.js
+++ main.js
@@ -53,11 +53,19 @@ function handleCollisions() {
                 (bird.y > canvas.height - obstaclesArray[i].bottom &&
                     bird.y + bird.height < canvas.height))) {
 
+ 
+
             ctx.drawImage(bang, bird.x, bird.y, 50, 50);
             ctx.font = "25px Georgia";
             ctx.fillStyle = 'black';
-            ctx.fillText('Game Over, your score is: ' + score, 160, canvas.height / 2 - 10);
-            return true;
-        }
-    }
+            
+var highScore= JSON.parse(localStorage.getItem('highScoreBirds'));
+if(highScore)
+            {
+if(score>highScore)
+highScore=score;
+localStorage.setItem('highScoreBirds',JSON.stringify(highScore));
+ctx.fillText('Game Over,High Score'+highScore+'   your score is: ' + score, 160, canvas.height / 2 - 10);
 }
+return true;
+        }
